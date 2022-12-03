c2cae5c11b5a66463b02db2f787b268bedaa552d
Lucas <weber-lucas@web.de>
2022-12-03 18:13:09 +0100

    day 3 add some timing measurement

--- 03/Solution.py
+++ 03/Solution.py
@@ -58,19 +58,58 @@ def unholy_main1():
 def unholy_main2():
     print(sum(map(lambda char: ord(char) - 38 if ord(char) < 97 else ord(char) - 96, (set(group[0].rstrip()).intersection(group[1].rstrip(), group[2].rstrip()).pop() for group in zip(*[iter(open('input.txt').readlines())]*3)))))
 
+# Functions to time ----------------------------------------------------------------------------------------------------
 
-def time_the_functions(repetitions: int = 10000):
-    main1_timed = timeit.timeit(inspect.getsource(main1), number=repetitions)
-    print(main1_timed)
-    main2_timed = timeit.timeit(inspect.getsource(main2), number=repetitions)
-    unholy_main1_timed = timeit.timeit(inspect.getsource(unholy_main1), number=repetitions)
-    unholy_main2_timed = timeit.timeit(inspect.getsource(unholy_main2), number=repetitions)
+
+def t_main1():
+    result = 0
+
+    # get the rucksacks
+    rucksacks = read_input()
+    # split the rucksacks and make a set intersection to find the common items
+    common_items = [set(rucksack[:len(rucksack)//2]).intersection(rucksack[len(rucksack)//2:]).pop()
+                    for rucksack in rucksacks]
+
+    # find the common items
+    result = sum(map(score, common_items))
+
+
+def t_main2():
+    result = 0
+
+    # get the rucksacks
+    rucksacks = read_input()
+
+    # assert that we have groups of three
+    assert len(rucksacks) % 3 == 0, 'Something is fishy.'
+
+    # group the rucksacks into groups of three and find common item
+    common_items = [set(rucksacks[idx*3]).intersection(*rucksacks[idx*3:idx*3+3]).pop()
+                    for idx in range(len(rucksacks)//3)]
+
+    # find the score
+    result = sum(map(score, common_items))
+
+
+def t_unholy_main1():
+    sum(map(lambda char: ord(char) - 38 if ord(char) < 97 else ord(char) - 96, (set(rucksack.rstrip()[:len(rucksack)//2]).intersection(rucksack[len(rucksack)//2:]).pop() for rucksack in open('input.txt').readlines())))
+
+
+def t_unholy_main2():
+    sum(map(lambda char: ord(char) - 38 if ord(char) < 97 else ord(char) - 96, (set(group[0].rstrip()).intersection(group[1].rstrip(), group[2].rstrip()).pop() for group in zip(*[iter(open('input.txt').readlines())]*3))))
+
+
+def time_the_functions(repetitions: int = 1000):
+    main1_timed = timeit.Timer(lambda: t_main1()).timeit(repetitions)
+    main2_timed = timeit.Timer(lambda: t_main2()).timeit(repetitions)
+    unholy_main1_timed = timeit.Timer(lambda: t_unholy_main1()).timeit(repetitions)
+    unholy_main2_timed = timeit.Timer(lambda: t_unholy_main2()).timeit(repetitions)
 
     print(f'The functions took: '
-          f'\n\tmain1:\t\t\t{main1_timed:.2E}'
-          f'\n\tmain2:\t\t\t{main2_timed:.2E}'
-          f'\n\tunholy_main1:\t{unholy_main1_timed:.2E}'
-          f'\n\tunholy_main2:\t{unholy_main2_timed:.2E}')
+          f'\n\tmain1:\t\t\t{main1_timed/repetitions*1_000_000:.2f} \u03BCs per call.'
+          f'\n\tmain2:\t\t\t{main2_timed/repetitions*1_000_000:.2f} \u03BCs per call.'
+          f'\n\tunholy_main1:\t{unholy_main1_timed/repetitions*1_000_000:.2f} \u03BCs per call.'
+          f'\n\tunholy_main2:\t{unholy_main2_timed/repetitions*1_000_000:.2f} \u03BCs per call.')
 
 
 if __name__ == '__main__':
