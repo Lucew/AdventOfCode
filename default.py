3b8cddf7f3ef382b52ddbb26dd9e04f31284e196
Lucas <weber-lucas@web.de>
2022-12-03 12:19:10 +0100

    init script added

---
+++ default.py
@@ -0,0 +1,22 @@
+def read_input(path: str = 'input.txt'):
+    inputs = []
+    with open(path) as filet:
+        for line in filet.readlines():
+            line = line.rstrip()
+            inputs.append(line)
+    return inputs
+
+
+def main1():
+    result = 0
+    print(f'The result for solution 1 is: {result}')
+
+
+def main2():
+    result = 0
+    print(f'The result for solution 2 is: {result}')
+
+
+if __name__ == '__main__':
+    main1()
+    main2()
