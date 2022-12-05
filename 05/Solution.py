e33d7e5d3f83fa3aedc28848dc75de5042f49d0a
Lucas <weber-lucas@web.de>
2022-12-05 05:50:20 +0100

    day 5 init

---
+++ 05/Solution.py
@@ -0,0 +1,28 @@
+from collections import Counter, defaultdict
+from heapq import heappop, heappush, heappushpop
+from functools import reduce
+from bisect import bisect_left, bisect_right
+
+
+def read_input(path: str = 'input.txt'):
+    inputs = []
+    with open(path) as filet:
+        for line in filet.readlines():
+            line = line.rstrip()
+            inputs.append(line)
+    return inputs
+
+
+def main1():
+    result = 0
+    print(f'The result for solution 1 is: {result}')
+
+
+def main2():
+    result = 0
+    print(f'The result for solution 2 is: {result}')
+
+
+if __name__ == '__main__':
+    main1()
+    main2()
