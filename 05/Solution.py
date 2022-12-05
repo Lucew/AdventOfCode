21cf7766b8a771bafff2e9ecc7af61a3a914cbde
Lucas <weber-lucas@web.de>
2022-12-05 06:47:12 +0100

    day 5 solved

--- 05/Solution.py
+++ 05/Solution.py
@@ -1,26 +1,69 @@
-from collections import Counter, defaultdict
-from heapq import heappop, heappush, heappushpop
-from functools import reduce
-from bisect import bisect_left, bisect_right
-
-
 def read_input(path: str = 'input.txt'):
-    inputs = []
     with open(path) as filet:
+
+        # get the crates on the ship
+        crates = []
+        line = ' '
+        while line:
+
+            # get the line
+            line = filet.readline().rstrip()
+
+            # check for empty line
+            if not line:
+                break
+
+            line = list(line[1::4])
+            crates.append(line)
+
+        # restructure the crates
+        tmp = [[] for _ in range(len(crates[-1]))]
+        for line in crates[:-1]:
+            for idx, ele in enumerate(line):
+                if ele != ' ':
+                    tmp[idx].append(ele)
+        crates = [list(reversed(stack)) for stack in tmp]
+
+        # get the commands
+        commands = []
         for line in filet.readlines():
             line = line.rstrip()
-            inputs.append(line)
-    return inputs
+            commands.append(list(int(ele) for ele in line.split()[1::2]))
+
+    return crates, commands
 
 
 def main1():
-    result = 0
-    print(f'The result for solution 1 is: {result}')
+
+    # get the input
+    crates, instructions = read_input()
+
+    # go through the instructions
+    for number, source, target in instructions:
+
+        # extend the target
+        crates[target-1].extend(reversed(crates[source-1][-number:]))
+
+        # delete the source
+        crates[source-1] = crates[source-1][:-number]
+
+    print(f'The result for solution 1 is: {"".join(ele[-1] for ele in crates)}')
 
 
 def main2():
-    result = 0
-    print(f'The result for solution 2 is: {result}')
+
+    # get the input
+    crates, instructions = read_input()
+
+    # go through the instructions
+    for number, source, target in instructions:
+        # extend the target
+        crates[target - 1].extend(crates[source - 1][-number:])
+
+        # delete the source
+        crates[source - 1] = crates[source - 1][:-number]
+
+    print(f'The result for solution 1 is: {"".join(ele[-1] for ele in crates)}')
 
 
 if __name__ == '__main__':
