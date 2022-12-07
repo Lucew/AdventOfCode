6f9baa049e61de19ac2452f7425e5333784af6f5
Lucas <weber-lucas@web.de>
2022-12-07 07:15:12 +0100

    day 7 make clean solution

--- 07/Solution.py
+++ 07/Solution.py
@@ -1,7 +1,4 @@
-from collections import Counter, defaultdict
-from heapq import heappop, heappush, heappushpop
-from functools import reduce
-from bisect import bisect_left, bisect_right
+import collections
 
 
 def read_input(path: str = 'input.txt'):
@@ -13,150 +10,141 @@ def read_input(path: str = 'input.txt'):
     return inputs
 
 
-def main1():
+def create_filesystem(instructions: list):
+
+    # introduce a dict that we will use for the file systems
     file_system = dict()
-    stack = []
-    inputs = read_input()
-    for line in inputs:
-        # check the command
-        # change directory
+
+    # introduce a stack that we will use for the current path
+    stack = collections.deque()
+
+    # go through the instructions one by one
+    for line in instructions:
+
+        # search for change directory commands that start with $ cd
         if line[0] == '$' and line[1] == 'cd':
+
+            # check for return to highest level
             if line[2] == '/':
+
+                # clear the stack
                 stack.clear()
+
+                # set us to the highest level
                 stack.append('/')
+
+            # check for level up command
             elif line[2] == '..':
+
+                # check that we are not already at the highest level
                 if stack[-1] != '/':
+                    # delete the last element from the stack
                     stack.pop()
             else:
                 stack.append(line[2])
 
-        elif line[0].isnumeric():
+        # check for a line that has file sizes in it
+        elif line[0][0].isdigit():
 
-            # go through the stack and create dict
+            # go through the file system and add the new files
             cur_path = file_system
             for idx, ele in enumerate(stack):
 
-                # add element to the file system
+                # initialize the current level if we have not yet visited
                 if ele not in cur_path:
+
+                    # every directory has two parts: the subdirectories and the files
+                    # at this level
                     cur_path[ele] = {'sub': dict(), 'files': [0]}
 
-                # go further
+                # check whether we reached the end of our stack
                 if idx == len(stack) - 1:
                     cur_path = cur_path[ele]
+
+                # update our filesystem to the current level
                 else:
                     cur_path = cur_path[ele]['sub']
 
             # append the file size
             cur_path['files'].append(int(line[0]))
 
-    # go through the file system and add up
-    def recursive_walk(folder):
+    return file_system
 
-        # get folder size
-        disc_space = sum(folder['files'])
 
-        # check whether the folder is empty
-        if len(folder['sub']) == 0:
-            return disc_space, disc_space if disc_space <= 100000 else 0
+# go through the file system and add up
+def recursive_walk(folder, current_path, folder_sizes, max_size):
 
-        # go deeper
-        accumulated_space = 0
-        for sub in folder['sub'].values():
-            sub_space, sub_smaller = recursive_walk(sub)
+    # get the space of all files at the current folder level
+    folder_size = sum(folder['files'])
 
-            # add up the disc space
-            disc_space += sub_space
-            accumulated_space += sub_smaller
+    # check whether the folder has not further subdirectories
+    # so we can return
+    if len(folder['sub']) == 0:
 
-        return disc_space, accumulated_space + disc_space if disc_space <= 100000 else accumulated_space
+        # add the folder size to our list of folder sizes
+        folder_sizes["/".join(current_path)] = folder_size
 
-    result = recursive_walk(file_system['/'])[1]
-    print(f'The result for solution 1 is: {result}')
+        # return the folder space and start accumulation of folders
+        # bigger than the specified max_size
+        return folder_size, folder_size if folder_size <= max_size else 0
+
+    # we have subdirectories so we need to recurse deeper first
+    accumulated_smaller = 0
+    for name, sub in folder['sub'].items():
+
+        # append the current name to the current path
+        current_path.append(name)
+
+        # go recursively
+        sub_space, sub_smaller = recursive_walk(sub, current_path, folder_sizes, max_size)
+
+        # reset the current path
+        current_path.pop()
+
+        # accumulate overall folder_size with subdirectories and space of folders smaller than max_size
+        folder_size += sub_space
+        accumulated_smaller += sub_smaller
+
+    # add the current accumulated folder size to the outer scope dict
+    folder_sizes["/".join(current_path)] = folder_size
+
+    return folder_size, accumulated_smaller + folder_size if folder_size <= max_size else accumulated_smaller
+
+
+def main1():
+
+    # get the instructions
+    instructions = read_input()
+
+    # get the filesystem
+    file_system = create_filesystem(instructions)
+
+    # walk the filesystem
+    folder_sizes = dict()
+    current_path = collections.deque(['/'])
+    disc_space, accumulated_smaller = recursive_walk(file_system['/'], current_path, folder_sizes, 100000)
+    print(f'The result for solution 1 is: {accumulated_smaller}')
 
 
 def main2():
 
-    # make the file system
-    file_system = dict()
-    stack = []
-    inputs = read_input()
-    for line in inputs:
-        # check the command
-        # change directory
-        if line[0] == '$' and line[1] == 'cd':
-            if line[2] == '/':
-                stack.clear()
-                stack.append('/')
-            elif line[2] == '..':
-                if stack[-1] != '/':
-                    stack.pop()
-            else:
-                stack.append(line[2])
+    # get the instructions
+    instructions = read_input()
 
-        elif line[0].isnumeric():
+    # get the filesystem
+    file_system = create_filesystem(instructions)
 
-            # go through the stack and create dict
-            cur_path = file_system
-            for idx, ele in enumerate(stack):
-
-                # add element to the file system
-                if ele not in cur_path:
-                    cur_path[ele] = {'sub': dict(), 'files': [0]}
-
-                # go further
-                if idx == len(stack) - 1:
-                    cur_path = cur_path[ele]
-                else:
-                    cur_path = cur_path[ele]['sub']
-
-            # append the file size
-            cur_path['files'].append(int(line[0]))
-
-    # go through the file system and add up
-    def recursive_walk(folder, current_path, folder_sizes):
-
-        # get the sum of files
-        disc_space = sum(folder['files'])
-
-        # check whether the folder is empty
-        if len(folder['sub']) == 0:
-            # add the folder size to the outer scope dict
-            folder_sizes["/".join(current_path)] = disc_space
-
-            return disc_space, disc_space if disc_space <= 100000 else 0
-
-        # go deeper
-        accumulated_space = 0
-        for name, sub in folder['sub'].items():
-
-            # append the curren name to the path
-            current_path.append(name)
-
-            # go recursively
-            sub_space, sub_smaller = recursive_walk(sub, current_path, folder_sizes)
-
-            # delete the name from the path
-            current_path.pop()
-
-            # add up the disc space
-            disc_space += sub_space
-            accumulated_space += sub_smaller
-
-        # add the folder size to the outer scope dict
-        folder_sizes["/".join(current_path)] = disc_space
-
-        return disc_space, accumulated_space + disc_space if disc_space <= 100000 else accumulated_space
-
-    # a variable to save the folder size
-    sizes = dict()
-    result = recursive_walk(file_system['/'], ['/'], sizes)[0]
+    # walk the filesystem
+    folder_sizes = dict()
+    current_path = collections.deque(['/'])
+    disc_space, accumulated_smaller = recursive_walk(file_system['/'], current_path, folder_sizes, 100000)
 
     # check the amount we need to free
-    need_to_free = 30000000 - (70000000 - result)
+    need_to_free = 30000000 - (70000000 - disc_space)
 
     # go through the sizes and find the smallest one larger than the space we need to free
     min_space = 70000000
-    for value in sizes.values():
+    for value in folder_sizes.values():
         if value > need_to_free:
             min_space = min(min_space, value)
 
