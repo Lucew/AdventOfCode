708746717affeda28a110aad1d24dc0ac3f4ab48
Lucas <weber-lucas@web.de>
2022-12-11 14:06:18 +0100

    day 11 even better solution

--- 11/Solution.py
+++ 11/Solution.py
@@ -1,6 +1,7 @@
 from collections import deque
 import functools
 from heapq import heappop, heappush, heapify
+from functools import reduce
 
 
 def read_input(path: str = 'input.txt'):
@@ -229,6 +230,10 @@ def main2():
     2) For every operation we update each dict item with the rules given above
     3) We check if the right dict value is 0 if we do the passing
 
+    One could even do better:
+    As all of the apes modulo operations are itself primes, we do not have to keep a dict per item. Only keep
+    the modulo of the multiplications of all ape primes. See main3 for improvement.
+
     :return:
     """
     # read the inputs
@@ -266,6 +271,93 @@ def main2():
     print(f'The result for solution 2 is: {biggest[0] * biggest[1]}')
 
 
+def read_input3(path: str = 'input.txt'):
+
+    # read the lines
+    inputs = []
+    with open(path) as filet:
+        for line in filet.readlines():
+            line = line.rstrip()
+            inputs.append(line)
+
+    # split them by monkeys
+    inputs = [inputs[idx*7:(idx+1)*7-1] for idx in range(len(inputs)//7+1)]
+
+    # make a set of primes
+    primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}
+
+    # parse the outputs
+    monkeys = []
+    for idx, monkey in enumerate(inputs):
+
+        # check the monkey length
+        assert len(monkey) == 6, 'Monkey is too long.'
+
+        # check the monkey number
+        assert int(monkey[0].split(' ')[-1][:-1]) == idx, 'Monkey number mix up.'
+
+        # get the list of items
+        items = deque([int(ele) for ele in monkey[1].split(':')[-1].split(',')])
+
+        # get the operation and transform it
+        operation = monkey[2].split(':')[-1].split(' = ')[-1]
+
+        # get the test
+        assert monkey[3].startswith('  Test: divisible by '), 'Something with the test ist off.'
+        test = int(monkey[3].split(' ')[-1])
+        assert test in primes, 'Test is not a prime.'
+
+        # get the target
+        assert monkey[4].startswith('    If true: throw to monkey '), 'Target True not specified.'
+        assert monkey[5].startswith('    If false: throw to monkey '), 'Target False not specified.'
+        target = [int(monkey[5].split(' ')[-1]), int(monkey[4].split(' ')[-1])]
+        assert target[0] != idx and target[1] != idx and 0 <= target[0] < len(inputs) and 0 <= target[1] < len(inputs), 'Monkey target are wild.'
+
+        # build the new monkey array
+        monkeys.append([items, operation, test, target])
+
+    # find the unique modulo operator
+    primes = {monkey[2] for monkey in monkeys}
+    modulo = reduce(lambda x, y: x*y, primes)
+
+    return monkeys, modulo
+
+
+def main3():
+
+    # get the inputs
+    inputs, modulo = read_input3()
+
+    # make a counter
+    counter = [0] * len(inputs)
+
+    # make steps
+    for _ in range(10000):
+        for idx, monkey in enumerate(inputs):
+
+            # go through all items the monkey has
+            while monkey[0]:
+                # increase inspection counter of this monkey
+                counter[idx] += 1
+
+                # get the item
+                item = monkey[0].popleft()
+
+                # calculate the new worry level (after inspection)
+                worry = modulo_evaluator(monkey[1], item, modulo)
+
+                # make the check and get the target
+                target = monkey[3][worry % monkey[2] == 0]
+
+                # throw it to the monkey
+                inputs[target][0].append(worry)
+
+    # get the k biggest apes
+    biggest = get_biggest_k(counter, 2)
+    print(f'The result for solution 2 is: {biggest[0] * biggest[1]}')
+
+
 if __name__ == '__main__':
     main1()
     main2()
+    main3()
