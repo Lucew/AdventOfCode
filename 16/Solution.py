e0708801581c536c831f4244724a8a7429ba5ee9
Lucas <weber-lucas@web.de>
2022-12-16 21:25:25 +0100

    day 16 fast solution

--- 16/Solution.py
+++ 16/Solution.py
@@ -85,6 +85,37 @@ def get_shortest_connections(worthy_valves: set, graph: dict):
     return shortest_path
 
 
+def traverse_everything(shortest_path, graph, time):
+
+    # save all paths and their maximum possible flow
+    pathes = defaultdict(lambda: -1)
+
+    # traverse our valves in bfs fashion
+    queue = deque([('AA', 0, time, set())])
+    while queue:
+
+        # pop the most recent node
+        position, accumulated_flow, time, visited = queue.popleft()
+
+        # get our neighbours that we can reach in time
+        neighbours = (neighbor for neighbor in shortest_path[position]
+                      if neighbor not in visited and shortest_path[position][neighbor] < time)
+
+        # update the maximum
+        if pathes[frozenset(visited)] < accumulated_flow:
+            pathes[frozenset(visited)] = accumulated_flow
+
+        # append the neighbours
+        for neighbor in neighbours:
+            # get the new flow
+            new_flow = (time - shortest_path[position][neighbor] - 1) * graph[neighbor][0]
+
+            # make the new set
+            new_set = visited | {neighbor}
+            queue.append((neighbor, accumulated_flow + new_flow, time - shortest_path[position][neighbor] - 1, new_set))
+    return pathes
+
+
 def main1():
     # parse the input
     inputs = read_input()
@@ -95,42 +126,13 @@ def main1():
     # get the shortest path between worthy valves
     shortest_path = get_shortest_connections(non_zero, inputs)
 
-    # traverse our valves in bfs fashion
-    queue = deque([('AA', 0, 30, set())])
-    result = 0
-    while queue:
+    # get all possible paths and their maximum flow
+    pathes = traverse_everything(shortest_path, inputs, 30)
 
-        # pop the most recent node
-        position, accumulated_flow, time, visited = queue.popleft()
-
-        # get our neighbours that we can reach in time
-        neighbours = [neighbor for neighbor in shortest_path[position]
-                      if neighbor not in visited and shortest_path[position][neighbor] < time]
-
-        # if we don't have neighbours left, we cann update the result
-        if not neighbours:
-            result = max(result, accumulated_flow)
-            continue
-
-        # append the neighbours
-        for neighbor in neighbours:
-            # get the new flow
-            new_flow = (time - shortest_path[position][neighbor] - 1)*inputs[neighbor][0]
-
-            # make the new set
-            new_set = visited | {neighbor}
-            queue.append((neighbor, accumulated_flow + new_flow, time-shortest_path[position][neighbor] - 1, new_set))
-    print(f'The result for solution 1 is: {result}')
-
-
-# make a greedy estimation of what is still possible
-def upper_bound(time, graph):
-    # open all valves at the same time
-    return sum(time*value[0] for value in graph.values())
+    print(f'The result for solution 1 is: {max(pathes.values())}')
 
 
 def main2():
-
     # parse the input
     inputs = read_input()
 
@@ -140,77 +142,12 @@ def main2():
     # get the shortest path between worthy valves
     shortest_path = get_shortest_connections(non_zero, inputs)
 
-    # make a cache
-    cache = defaultdict(lambda: -1)
+    # get all possible paths and their maximum flow
+    pathes = traverse_everything(shortest_path, inputs, 26)
 
-    # traverse our valves in bfs fashion
-    queue = [(0, (('AA', 26), ('AA', 26)), set())]
-    result = 0
-    counter = 0
-    while queue:
-
-        # increase the counter
-        counter += 1
-
-        # do something all 100_000 steps
-        if counter % 100_000 == 0:
-
-            # clean the queue
-            queue = [ele for ele in queue if ele[0] - upper_bound(max(actor[1] for actor in ele[1]), inputs) < result]
-
-            # make heap again
-            heapify(queue)
-
-            # print the result
-            print(counter/1_000_000, 'M', result)
-
-        # pop the most recent node
-        # print(queue)
-        accumulated_flow, actors, visited = heappop(queue)
-
-        # find the actor with the most time left
-        max_idx = 0
-        max_time = actors[0][1]
-        for idx, (position, time) in enumerate(actors):
-            if time > max_time:
-                max_idx = idx
-                max_time = time
-
-        # get the time and position for that actor
-        time = max_time
-        position = actors[max_idx][0]
-
-        # check whether our reachable flow is to low
-        if accumulated_flow - upper_bound(time, inputs) > result:
-            continue
-
-        # make a neighbour generator
-        neighbours = (neighbor for neighbor in shortest_path[position]
-                      if neighbor not in visited and shortest_path[position][neighbor] < time)
-
-        # append the neighbours
-        for neighbor in neighbours:
-            # get the new flow
-            new_flow = accumulated_flow - (time - shortest_path[position][neighbor] - 1) * inputs[neighbor][0]
-
-            # make the new set
-            new_set = visited | {neighbor}
-
-            # compute the new time
-            new_time = time-shortest_path[position][neighbor] - 1
-
-            # check whether our reachable flow is to low
-            if new_flow - upper_bound(time, inputs) > result:
-                continue
-
-            # update the flow
-            if new_flow < result:
-                result = new_flow
-
-            # make the new actors
-            new_actors = tuple(actor if idx != max_idx else (neighbor, new_time) for idx, actor in enumerate(actors))
-            heappush(queue, (new_flow, new_actors, new_set))
-
+    # get the maximum of all combinations that do not overlap valves
+    result = max(
+        flow1 + flow2 for path1, flow1 in pathes.items() for path2, flow2 in pathes.items() if not path1 & path2)
     print(f'The result for solution 2 is: {result}')
 
 
