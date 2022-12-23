388dac061e1a25b3557ad79ced5596419d848862
Lucas <weber-lucas@web.de>
2022-12-23 03:53:56 +0100

    day 22 shameless hard coding

--- 22/Solution.py
+++ 22/Solution.py
@@ -75,46 +75,17 @@ def next_position(rx: int, cx: int, direction: str,
     return rx, cx
 
 
-def wrap_around(rx: int, cx: int, direction: str,
-                  row_minmax: dict[int: list[int, int]], col_minmax: dict[int: list[int, int]]):
-
-    # calculate the new position based on the direction
-    if direction == '>':
-        cx += 1
-        # check whether we wrapped around
-        if cx > row_minmax[rx][1]:
-            cx = row_minmax[rx][0]
-    elif direction == '<':
-        cx -= 1
-        # check whether we wrapped around
-        if cx < row_minmax[rx][0]:
-            cx = row_minmax[rx][1]
-    elif direction == '^':
-        rx -= 1
-        # check whether we wrapped around
-        if rx < col_minmax[cx][0]:
-            rx = col_minmax[cx][1]
-    elif direction == 'v':
-        rx += 1
-        # check whether we wrapped around
-        if rx > col_minmax[cx][1]:
-            rx = col_minmax[cx][0]
-    else:
-        raise NotImplementedError
-    return rx, cx
-
-
 def main1():
 
     # get the input readings
     free, blocked, path, row_minmax, col_minmax = read_input()
 
     # extract the commands from the path
-    path = re.findall(r'[0-9]*[A-Z]', path + '.')
+    path = re.findall(r'[0-9]*[A-Z]', path + 'A')
 
     # turning dict for quick turning
     turning = {'R': {'>': 'v', '<': '^', 'v': '<', '^': '>'}, 'L': {'>': '^', '<': 'v', 'v': '>', '^': '<'},
-               '.': {'>': '>', '<': '<', 'v': 'v', '^': '^'}}
+               'A': {'>': '>', '<': '<', 'v': 'v', '^': '^'}}
 
     # find the starting point
     start = row_minmax[0][0]
@@ -154,23 +125,145 @@ def main1():
     print(f'The result for solution 1 is: {result}')
 
 
+def wrap_around(rx: int, cx: int, direction: str,
+                row_minmax: dict[int: list[int, int]], col_minmax: dict[int: list[int, int]], lenght=50):
+
+    # calculate the new position based on the direction
+    if direction == '>':
+        cx += 1
+        # check whether we wrapped around
+        if cx > row_minmax[rx][1]:
+
+            if rx < 50:
+                cx = 99
+                rx = 149 - rx
+                direction = '<'
+            elif rx < 100:
+                cx = rx + 50
+                rx = 49
+                direction = '^'
+            elif rx < 150:
+                cx = 149
+                rx = 149 - rx
+                direction = '<'
+            elif rx < 200:
+                cx = rx - 100
+                rx = 149
+                direction = '^'
+            else:
+                raise NotImplementedError
+    elif direction == '<':
+        cx -= 1
+        # check whether we wrapped around
+        if cx < row_minmax[rx][0]:
+
+            if rx < 50:
+                cx = 0
+                rx = 149 - rx
+                direction = '>'
+            elif rx < 100:
+                cx = rx - 50
+                rx = 100
+                direction = 'v'
+            elif rx < 150:
+                cx = 50
+                rx = 149-rx
+                direction = '>'
+            elif rx < 200:
+                cx = rx - 100
+                rx = 0
+                direction = 'v'
+            else:
+                raise NotImplementedError
+    elif direction == '^':
+        rx -= 1
+        # check whether we wrapped around
+        if rx < col_minmax[cx][0]:
+
+            if cx < 50:
+                rx = 50 + cx
+                cx = 50
+                direction = '>'
+            elif cx < 100:
+                rx = cx + 100
+                cx = 0
+                direction = '>'
+            elif cx < 150:
+                rx = 199
+                cx = cx - 100
+                direction = '^'
+            else:
+                raise NotImplementedError
+    elif direction == 'v':
+        rx += 1
+        # check whether we wrapped around
+        if rx > col_minmax[cx][1]:
+            if cx < 50:
+                cx = cx + 100
+                rx = 0
+                direction = 'v'
+            elif cx < 100:
+                rx = cx + 100
+                cx = 49
+                direction = '<'
+            elif cx < 150:
+                rx = cx - 50
+                cx = 99
+                direction = '<'
+            else:
+                raise NotImplementedError
+    else:
+        raise NotImplementedError
+    return rx, cx, direction
+
+
 def main2():
 
     # get the input readings
     free, blocked, path, row_minmax, col_minmax = read_input()
 
     # extract the commands from the path
-    path = re.findall(r'[0-9]*[A-Z]', path + '.')
+    path = re.findall(r'[0-9]*[A-Z]', path + 'A')
 
     # turning dict for quick turning
     turning = {'R': {'>': 'v', '<': '^', 'v': '<', '^': '>'}, 'L': {'>': '^', '<': 'v', 'v': '>', '^': '<'},
-               '.': {'>': '>', '<': '<', 'v': 'v', '^': '^'}}
+               'A': {'>': '>', '<': '<', 'v': 'v', '^': '^'}}
 
     # find the starting point
     start = row_minmax[0][0]
     while (0, start) in blocked:
         start += 1
+
+    # go through the maze
     position = [0, start, '>']
+    for command in path:
+
+        # get the amount of steps
+        steps = int(command[:-1])
+
+        # make the steps
+        for step in range(steps):
+
+            # get the new position
+            rx, cx, direction = wrap_around(*position, row_minmax, col_minmax)
+
+            # check whether the way is blocked
+            if (rx, cx) in blocked:
+                break
+
+            # check whether we can go there
+            elif (rx, cx) in free:
+                position[0] = rx
+                position[1] = cx
+                position[2] = direction
+
+            else:
+                print(rx, cx)
+                raise ValueError
+
+
+        # make the rotation
+        position[2] = turning[command[-1]][position[2]]
 
     # make a dict for value translation of the direction
     value = {'>': 0, '<': 2, 'v': 1, '^': 3}
